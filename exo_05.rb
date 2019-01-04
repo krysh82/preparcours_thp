c6303857ecf51fd192c1d4725875b2a95a0a7f0f
krysh@82 <crysna@gmail.com>
2019-01-04 19:10:44 +0100

    ajout de commentaires

--- exo_05.rb
+++ exo_05.rb
@@ -1,42 +1,46 @@
-#{} permet de vérifier les informations inscrites et faire les calculs 
+#{} string interpolation - permet de vérifier les informations inscrites et faire les calculs 
 #output a string on the console
 puts "On va compter le nombre d'heures de travail à THP"
 
 #output a string and the result of the operation between the hash
 puts "Travail :  #{10 * 5 * 11}"
 
+#affiche le nombre de minutes
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
 
+#affiche la chaine de caractères qui est entre ""
 puts "Et en secondes ?"
 
-
+#affiche le résultat de l'opération
 puts 10 * 5 * 11 * 60 * 60
 
 
-
+#affiche la chaine de caractère et vérifie la condition
 puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
 
-
-
+#vérifie la condition et affiche le résulta sous forme booléenne
 puts 3 + 2 < 5 - 7
 
 
-
+#affiche la chaine de caractères qui est entre "" et affiche le résultat dans le hash
 puts "Ca fait combien 3 + 2 ? #{3 + 2}"
 
+#affiche la chaine de caractères qui est entre "" et affiche le résultat dans le hash
 puts "Ca fait combien 5 - 7 ? #{5 - 7}"
 
 
-
+#affiche la chaine de caractères qui est entre ""
 puts "Ok, c'est faux alors !"
 
 
-
+#affiche la chaine de caractères qui est entre ""
 puts "C'est drôle ça, faisons-en plus :"
 
-
+#affiche la chaine de caractères qui est entre "" et vérifie la condition dans le hash
 puts "Est-ce que 5 est plus grand -2 ? #{5 > -2}"
 
+#affiche la chaine de caractères qui est entre "" et vérifie la condition dans le hash
 puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
 
+#affiche la chaine de caractères qui est entre "" et vérifie la condition dans le hash
 puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
